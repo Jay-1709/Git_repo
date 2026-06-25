4fe555974764d5cd552aa2ca5c9b252b81088e66
Geek <jk0690025@gmail.com>
2026-06-25 11:23:52 +0530

    First_version_1

---
+++ first_repo.py
@@ -0,0 +1,8 @@
+# Your first line of Python code
+# Sample list containing lists
+list_of_lists = [[1, 2, 3], [], [4, 5], [], [6, 7, 8], []]
+
+
+#print(len(list_of_lists[2]))
+
+non_empty_list = [val for val in list_of_lists if len(list_of_lists) > 0]
