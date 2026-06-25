f02ec89ab291b001cea2fd06712a2a54067d9770
Geek <jk0690025@gmail.com>
2026-06-25 11:52:27 +0530

    Add empty list count

--- first_repo.py
+++ first_repo.py
@@ -2,7 +2,11 @@
 # Sample list containing lists
 list_of_lists = [[1, 2, 3], [], [4, 5], [], [6, 7, 8], []]
 
-
 #print(len(list_of_lists[2]))
 
-non_empty_list = [val for val in list_of_lists if len(list_of_lists) > 0]
+non_empty_list = [val for val in list_of_lists if len(val) > 0]
+print(f"Non empty list: {non_empty_list}")
+
+#Adding empty list count
+empty_list_count = list_of_lists.count([])
+print(f"Number of empty list in list is {empty_list_count}")
